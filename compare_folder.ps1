782f00b36d4834fbf7d76cef04d40852801ef933
Jason Whittle <jwhittle@bastiansolutions.com>
2017-12-29 17:02:51 -0500

    1st update

--- compare_folder.ps1
+++ compare_folder.ps1
@@ -1,13 +1,15 @@
 $dir1 = 'C:\Users\jwhittle\Desktop\WS01\WS01\'
 $dir2 = 'C:\Users\jwhittle\Desktop\WS01\WS11\'
-$d1 = get-childitem -path $dir1 -recurse
-$d2 = get-childitem -path $dir2 -recurse
+$Exclude = @("*.txt", "*.InstallLog")
+$d1 = get-childitem -path $dir1 -Recurse -Exclude $Eliminate #| ? {$_.FullName -inotmatch 'log*' }
+$d2 = get-childitem -path $dir2 -Recurse -Exclude $Eliminate #| ? {$_.FullName -inotmatch 'log*' }
+Write-Host "Number of files to check:" $d1.count
 <#echo "$dir1\$d1"#>
 foreach($file in $d1){
     #echo "$dir1$file"
     $prod = "$dir1$file"
     $bak = "$dir2$file"
-    echo $prod
+    #echo $prod
     #echo $bak
     #echo $(get-content "$prod")
     #echo $(get-content "$bak")
@@ -15,7 +17,7 @@ foreach($file in $d1){
     if(Compare-Object $(get-content "$prod") $(get-content "$bak")){
         echo $prod" --- File OUT of sync"
     }Else{
-       # echo $file
+       #echo $file
        # "File IN SYNC"
     }
 }
