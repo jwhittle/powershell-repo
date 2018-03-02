9585b1aa6444787ff51996dccd1f782113d134fe
Jason Whittle <jwhittle@bastiansolutions.com>
2018-03-02 09:09:05 -0500

    Inital upload
    Loaded an example file,  and my first start at the script

---
+++ Carton Release/crap.ps1
@@ -0,0 +1,14 @@
+﻿#$data = Get-Content 'C:\Users\jwhittle\Desktop\c_r_a_p\Carton Release.txt'
+
+
+#foreach ($line in $data){
+    #Select-String -Path $line -Pattern "Ready to Process Request Barcode"
+    #Write-host "$line"
+#}
+
+#$F = 
+Select-String -Path 'C:\Users\jwhittle\Desktop\c_r_a_p\Carton Release.txt' -Pattern "Ready to Process Request Barcode" -Context 0, 3 
+#| %{$data = $_.Split('-'); Write-host "'$($data[0])'"}
+
+#$F.count
+#($F)[0].context clear
