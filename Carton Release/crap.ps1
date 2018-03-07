6097d80dba57d03213c1a3e9b31bebcde8875545
Jason Whittle <jwhittle@bastiansolutions.com>
2018-03-07 17:32:04 -0500

    minor tweak

--- Carton Release/crap.ps1
+++ Carton Release/crap.ps1
@@ -5,20 +5,103 @@ $data = Get-Content $file
 #07:33:26.537 [1] INFO  Equipment 5 - 07:33:26:537 - SCAN  Queued UP For Scanner Carton PandA Verif TIME START  $Queued_up[5]
 #07:33:26.611 [1] INFO  Equipment 5 - 07:33:26:611 - GOOD Match at Carton PandA Verif. Barcode1 C7DT8ZI6I3F Barcode2 C7DT8ZI6I3 - [07:33:26:565]  $Good_match[6]
 #07:33:26.623 [1] INFO  Equipment 5 - 07:33:26:623 - Posting Process Scan results for Scanner Carton PandA Verif to controls for package tracking id - 98, barcode - C7DT8ZI6I3, destination - 99. $Post_[5]
+$matches = @{
 
 
+}
 
 
+$start_line = @()
+$counter = 0
 foreach ($line in $data){
-    #slect-String -Path $line -Pattern "Ready to Process Request Barcode"
-
     if ($line -match "SCAN  Queued UP For Scanner Carton PandA Verif TIME START"){
+        $Start_time_patern = " - (.*) - SCAN"
+        $Start_time = [regex]::Match($line, $Start_time_patern).Groups[1].Value
         
+        $sub_lines = 1
+        $end = 10
+        write-host "$counter $Start_time"
+        DO{            
+            $next_entry_num = $counter + $sub_lines
+            $next_entry = $data[$next_entry_num]
+            if ($next_entry -match "GOOD Match at Carton PandA Verif. Barcode1"){###
+                #pull time, and barcode1, barcode2
+                $write = 'Y'
+                $verify_time
+            }elseif($next_entry -match " - Barcode Mismatch at Carton PandA Verif. Barcode1 "){##
+                #pull time and ID
+                $write = 'Y'
+            }elseif($next_entry -match " - Posting Process Scan results for Scanner Carton PandA Verif to controls for package tracking id - "){
+                #pull time and ID
+                $write = 'Y'
+            }else{
+                $write ='N'
+        }
+        if ($write -eq 'Y'){
+            write-host "$next_entry"
+        }
+            
+            
+            
+            
+            
 
-        #Ready to Process parse -> $container_id
-        ##Pull container ID
-        $Start_time_patern = " - (.*) - GOOD "
-        $Start_time = [regex]::Match($line, $container_id_patern).Groups[1].Value
+
+            $sub_lines++
+        } While ($sub_lines -le $end)
+
+
+
+
+        
+        $next_entry = $data[$next_entry_num]
+        if ($next_entry -match "GOOD Match at Carton PandA Verif. Barcode1"){###
+            #pull time, and barcode1, barcode2
+            $write = 'N'
+        }elseif($next_entry -match "Found Request for Activity Tracking AUTOMATION "){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match "Ready to Process Request Barcode "){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match "Detected no read at Carton PandA Verif. Barcode1 "){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match " - Route Container "){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match " has been updated to released status - "){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match "Beginning to update status for container "){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match " Queued UP For Scanner Carton PandA TIME START"){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match " - Updating container "){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match " - Container Name "){
+            #pull time and ID
+            $write = 'N'
+        }elseif($next_entry -match " - Barcode Mismatch at Carton PandA Verif. Barcode1 "){##
+            #pull time and ID
+            $write = 'N'
+         }elseif($next_entry -match " - Looking for Released Carton with Carton Type "){
+            #pull time and ID
+            $write = 'N'
+        }else{
+            $write ='Y'
+        }
+        
+        
+        
+        
+        if ($write -eq 'Y'){
+        
+            #write-host "$counter $Start_time - $next_entry"
+        }
         
         ##Pull actual time - $time
         #$time_patern = "- \[(.*)\]"
@@ -73,6 +156,7 @@ foreach ($line in $data){
 #        }
         
     }
+    $counter++
 }
 
 
