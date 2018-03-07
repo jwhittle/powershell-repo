1bacbcc4aca0e4a555b30104d9dea28b2f2cf7cc
Jason Whittle <jwhittle@bastiansolutions.com>
2018-03-07 14:27:27 -0500

    Minor clean up

--- parsing examples/ecomm/ecomm.ps1
+++ parsing examples/ecomm/ecomm.ps1
@@ -17,24 +17,22 @@ $data = Get-Content $file
 
 $ip_list = @()                                                #create the blank array of ips
 foreach ($line in $data){
-    if ($line -like '*bingbot*'){                             #look form lines With Bingbot
-        if ($line -notlike '* 200 0 0 *'){                    #igore lines that end with '200 0 0'
-            $ip_patern = " - 443 Anonymous (.*) Mozilla/5.0"  #how do I find the ip
-            $ip = [regex]::Match($line, $ip_patern).Groups[1].Value #extract the ip
-            if ($ip -ne ''){                                  #clean up
-                $octet = $ip.Split('.')                       #get the 1st octet from the ip
-                if ($octet[0] -ne '10'){                      #ignore the 10.
-                    $ip_list += $ip                           #add it to the array
-                }
+    if (($line -like '*bingbot*') -and ($line -notlike '* 200 0 0 *')){  #look form lines With Bingbot and igore lines that end with '200 0 0'
+        $ip_patern = " - 443 Anonymous (.*) Mozilla/5.0"                 #how do I find the ip
+        $ip = [regex]::Match($line, $ip_patern).Groups[1].Value          #extract the ip
+        if ($ip -ne ''){                                                 #clean up
+            $octet = $ip.Split('.')                                      #get the 1st octet from the ip
+            if ($octet[0] -ne '10'){                                     #ignore the 10.
+                $ip_list += $ip                                          #add it to the array
             }
         }
     }
 }
 
 
-$ip_list = $ip_list | Sort-Object | Get-Unique               #get unique ips
+$ip_list = $ip_list | Sort-Object | Get-Unique                           #get unique ips
 #write-host $ip_list
-foreach ($ip in $ip_list){                                   #write them out
+foreach ($ip in $ip_list){                                               #write them out
     write-host "|$ip|"
     Add-Content $log "$ip"
 }
