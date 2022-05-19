777cd4a532aa73a1594c7fdd9ca6732bcdde66ad
jwhittle <whittle.j@gmail.com>
2022-05-19 16:41:04 -0400

    Tail logs script

---
+++ PROD - tail printserver.ps1
@@ -0,0 +1,16 @@
+﻿#DESCRIPTION: Open logs and tail -f
+#Ver: 1.0
+#Written by: Jason Whittle
+
+
+$path = "\\192.168.40.113\l$\Bastian Software\Logs\ExactaPrintServer\"
+$filename = Get-ChildItem "\\192.168.40.113\l$\Bastian Software\Logs\ExactaPrintServer\" | sort LastWriteTime | select -last 1
+
+$full_path = $path + $filename
+write-host $full_path
+
+#start powershell {
+    Get-Content $full_path -Tail 10 -Wait
+#    read-host
+    
+#    }
