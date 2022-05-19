29bf6d6a911ad7c171b7d54e68bf96353bd4807d
jwhittle <whittle.j@gmail.com>
2022-05-19 16:41:51 -0400

    RDP script

---
+++ RDP.ps1
@@ -0,0 +1,17 @@
+﻿function RDP{
+     Param
+    (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $Server,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $User,
+        [Parameter(Mandatory=$true, Position=2)]
+        [string] $Password
+    ) 
+    $Port="49982"
+
+    mstsc.exe /v:$Server /user $User /Password $Password
+
+}
+
+RDP -Server "192.168.126.18" -User "Administrator" -Password "Bss1234"
