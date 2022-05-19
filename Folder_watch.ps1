ac27f5b66cc1357a29e1e882c44e160c72a716e1
jwhittle <whittle.j@gmail.com>
2022-05-19 16:39:24 -0400

    Added Watch folder script

---
+++ Folder_watch.ps1
@@ -0,0 +1,24 @@
+﻿#Description:  Script to log changes to selected folder
+
+
+$pathToWatch = "C:\Program Files\Bastian Solutions"
+$pathToLog = "C:\temp\Folder_Watch_log.txt"
+### SET FOLDER TO WATCH + FILES TO WATCH + SUBFOLDERS YES/NO
+    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.Path = $pathToWatch
+    $watcher.Filter = "*.*"
+    $watcher.IncludeSubdirectories = $true
+    $watcher.EnableRaisingEvents = $true  
+
+### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
+    $action = { $path = $Event.SourceEventArgs.FullPath
+                $changeType = $Event.SourceEventArgs.ChangeType
+                $logline = "$(Get-Date), $changeType, $path"
+                Add-content $pathToLog -value $logline
+              }    
+### DECIDE WHICH EVENTS SHOULD BE WATCHED 
+    Register-ObjectEvent $watcher "Created" -Action $action
+    Register-ObjectEvent $watcher "Changed" -Action $action
+    Register-ObjectEvent $watcher "Deleted" -Action $action
+    Register-ObjectEvent $watcher "Renamed" -Action $action
+    while ($true) {sleep 5}
