7127904f4cd4cbfff6311307ac47fee8dc1966e0
jwhittle <whittle.j@gmail.com>
2024-03-13 10:28:19 -0400

    Version 3.0

--- folder compaire/compare_folders.ps1
+++ folder compaire/compare_folders.ps1
@@ -1,219 +1,68 @@
-﻿#Written By: Jason Whittle
-#version: 2.0
+﻿# Written By: Jason Whittle
+# Version: 3.0
+# Description: File Comparison script
 
 
-###### paramaters
+# Parameters
+$dir1 = 'C:\Users\whitt\Desktop\Test\A\'   #backup
+$dir2 = 'C:\Users\whitt\Desktop\Test\B\'   #prod
 
-$dir1 = 'C:\Users\jwhittle\Desktop\WS01\WS01\'
-$dir2 = 'C:\Users\jwhittle\Desktop\WS01\WS11\'
 
-$get_diff = 'Y'  #keep set to N unless you need to compaire files by checksum.  WARNING this will slow the script...   ALOT!!
-######
+function Compare-FolderVersions {
+    param(
+        [string]$folderPath1,
+        [string]$folderPath2
+    )
 
-#functions
+    Write-Host "Folder Comparison Report"
 
-function IsAscii([System.IO.FileInfo]$item){
-    begin 
-    { 
-        $validList = new-list byte
-        $validList.AddRange([byte[]] (10,13) )
-        $validList.AddRange([byte[]] (31..127) )
-    }
+    # Get all files recursively from both folders
+    $files1 = Get-ChildItem -Path $folderPath1 -Recurse | Where-Object {!$_.PSIsContainer}
+    $files2 = Get-ChildItem -Path $folderPath2 -Recurse | Where-Object {!$_.PSIsContainer}
 
-    process
-    {
-        try 
-        {
-            $reader = $item.Open([System.IO.FileMode]::Open)
-            $bytes = new-object byte[] 1024
-            $numRead = $reader.Read($bytes, 0, $bytes.Count)
+    # Iterate through each file and compare versions
+    foreach ($file1 in $files1) {
+        $relativePath = $file1.FullName.Substring($folderPath1.Length + 0)
+        
+        $file2 = $files2 | Where-Object {$_.FullName -eq (Join-Path -Path $folderPath2 -ChildPath $relativePath)}
 
-            for($i=0; $i -lt $numRead; ++$i)
-            {
-                if (!$validList.Contains($bytes[$i]))
-                    { return $false }
+        $fileVersion1 = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($file1.FullName).FileVersion
+        if (-not $fileVersion1) {
+            $fileContent1 = Get-Content $file1.FullName -Raw
+        }
+
+        if ($file2 -ne $null) {
+            $fileVersion2 = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($file2.FullName).FileVersion
+            if (-not $fileVersion2) {
+                $fileContent2 = Get-Content $file2.FullName -Raw
             }
-            $true
-        }
-        finally
-        {
-            if ($reader)
-                { $reader.Dispose() }
-        }
-    }
-}
 
-
-function Get-Compare_File_Version{
-    param([string]$prod,[string]$bak)
-    $ver_sync = @{}
-    $prod_ver = & '.\bin\sigcheck.exe' "-nobanner" "-n" $prod
-    $bak_ver =  & '.\bin\sigcheck.exe' "-nobanner" "-n" $bak
-
-    $ver_sync.Add('prod_ver',$prod_ver)
-    $ver_sync.Add('qa_ver',$bak_ver)
-
-    if(Compare-Object $prod_ver $bak_ver){
-        $ver_sync.Add('ver_sync','N') 
-    }Else{
-        $ver_sync.Add('ver_sync','Y') 
-    }
-    return $ver_sync
-
-}
-
-function Get-Compare_File{
-    param([string]$prod,[string]$bak)
-
-    #Check files for diff
-    if(Compare-Object $(get-content "$prod") $(get-content "$bak")){
-        $diff = 'N' 
-    }Else{
-        $diff = 'Y' 
-    }
-    return $diff
-
-}
-
-
-
-
-$d1 = get-childitem -File -path $dir1  -Recurse #`
-$data = @{}
-
-foreach($file in $d1){
-    $prod = "$dir1$file"
-    $bak = "$dir2$file"
-    
-    $info = @{}
-    
-    $info.Add('filename',$file)
-    $info.Add('prod_path',$prod)
-    $info.Add('qa_path',$bak)
-    
-    $ver_sync = @{}
-
-    #get versions of files and compaire
-    $ver_sync = Get-Compare_File_Version -prod $prod -bak $bak
-    
-    if ($get_diff -eq 'Y'){
-        if ($ver_sync.prod_ver -eq 'n/a'){
-            $diff = Get-Compare_File -prod $prod -bak $bak
-            $info.Add('diff_sync',$diff)
-        }
-    }
-
-    $info.Add('ver_sync',$ver_sync.ver_sync)
-    $info.Add('prod_ver',$ver_sync.prod_ver)
-    $info.Add('qa_ver',$ver_sync.qa_ver)
-    #build array of arrays
-    $data.Add($prod,$info)
-}
-
-
-
-function out-of-sync-table{
-    Add-Content 'report.html' '<div class="container">'
-    Add-Content 'report.html' '<h2>Version Mismatch</h2>'
-    Add-Content 'report.html' '<p>Files where the Versions are Different</p>' 
-    Add-Content 'report.html' '<table class="table table-striped">'
-    Add-Content 'report.html' '<thead>'
-    Add-Content 'report.html' '     <tr>'
-    Add-Content 'report.html' '          <TH>Filename</TH>'
-    Add-Content 'report.html' '          <TH>Prod Version</TH>'
-    Add-Content 'report.html' '          <TH>QA Version</TH>'
-    Add-Content 'report.html' '     </tr>'
-    Add-Content 'report.html' '</thead>'
-    Add-Content 'report.html' '<tbody>'
-    foreach($entry in $data.keys){
-        #write-host $entry
-        foreach($end_data in $data.$entry){ 
-            if ($end_data.ver_sync -eq 'N'){
-                Add-Content 'report.html' "     <TR>"
-                Add-Content 'report.html' "          <TD>", $end_data.filename ,"</TD>" -NoNewline
-                Add-Content 'report.html' "          <TD>", $end_data.prod_ver ,"</TD>" -NoNewline
-                Add-Content 'report.html' "          <TD>", $end_data.qa_ver ,"</TD>" -NoNewline
-                Add-Content 'report.html' "     </TR>"
-            }elseif($end_data.diff_sync -eq 'N'){
-                Add-Content 'report.html' "     <TR>"
-                Add-Content 'report.html' "          <TD>", $end_data.filename ,"</TD>" -NoNewline
-                Add-Content 'report.html' "          <TD colspan = '2'>Checksum missmatch </TD>" -NoNewline
-                #Add-Content 'report.html' "          <TD>", $end_data.qa_ver ,"</TD>" -NoNewline
-                Add-Content 'report.html' "     </TR>"
+            if ($fileVersion1 -ne $null -and $fileVersion2 -ne $null) {
+                if ($fileVersion1 -ne $fileVersion2) {
+                    Write-Host "Mismatch detected for file Version: $relativePath" -ForegroundColor Green
+                    Write-Host "Version in folder 1: $fileVersion1" -ForegroundColor Green
+                    Write-Host "Version in folder 2: $fileVersion2" -ForegroundColor Green
+                }
+            } elseif ($fileContent1 -ne $fileContent2) {
+                Write-Host "Mismatch detected for file content: $relativePath" -ForegroundColor Green
             }
+        } else {
+            Write-Host "File not found in folder 2: $relativePath" -ForegroundColor Green
         }
     }
-    Add-Content 'report.html' '</tbody>'
-    Add-Content 'report.html' "</TABLE>"
-    
-}
 
-function all-files-table{
-    Add-Content 'report.html' '<div class="container">'
-    Add-Content 'report.html' '<h2>All Files</h2>'
-    Add-Content 'report.html' '<p>All files scanned</p>' 
-    Add-Content 'report.html' '<table class="table table-striped">'
-    Add-Content 'report.html' '<thead>'
-    Add-Content 'report.html' '     <tr>'
-    Add-Content 'report.html' '          <TH>Filename</TH>'
-    Add-Content 'report.html' '          <TH>Prod Version</TH>'
-    Add-Content 'report.html' '          <TH>QA Version</TH>'
-    Add-Content 'report.html' '     </tr>'
-    Add-Content 'report.html' '</thead>'
-    Add-Content 'report.html' '<tbody>'
-    foreach($entry in $data.keys){
-        #write-host $entry
-        foreach($end_data in $data.$entry){
-            if ($end_data.ver_sync -eq 'N'){
-                Add-Content 'report.html' '<TR class="danger">'
-            }elseif($end_data.diff_sync -eq 'N'){
-                Add-Content 'report.html' '     <TR class="danger">'
-                Add-Content 'report.html' "          <TD>", $end_data.filename ,"</TD>" -NoNewline
-                Add-Content 'report.html' "          <TD colspan = '2'>Checksum missmatch </TD>" -NoNewline
-                #Add-Content 'report.html' "          <TD>", $end_data.qa_ver ,"</TD>" -NoNewline
-                Add-Content 'report.html' "     </TR>"
-            }else{
-                Add-Content 'report.html' "<TR>"
-            }
-            Add-Content 'report.html' "<TD>", $end_data.filename ,"</TD>" -NoNewline
-            Add-Content 'report.html' "<TD>", $end_data.prod_ver ,"</TD>" -NoNewline
-            Add-Content 'report.html' "<TD>", $end_data.qa_ver ,"</TD>" -NoNewline
-            #$end_data.prod_path $end_data.qa_path $end_data.ver_sync $end_data.prod_ver $end_data.qa_ver $end_data.diff
-            Add-Content 'report.html' "</TR>"
+    # Check for new files in folder 2 not present in folder 1
+    foreach ($file2 in $files2) {
+        $relativePath = $file2.FullName.Substring($folderPath2.Length + 0)
+        $file1 = $files1 | Where-Object {$_.FullName -eq (Join-Path -Path $folderPath1 -ChildPath $relativePath)}
+
+        if ($file1 -eq $null) {
+            Write-Host "New file found in folder 2: $relativePath" -ForegroundColor Green
         }
     }
-    Add-Content 'report.html' '</tbody>'
-    Add-Content 'report.html' "</TABLE>"
 }
 
+# Example usage:
+Compare-FolderVersions -folderPath1 $dir1 -folderPath2 $dir2
 
 
-#CREATE HTML HERE
-if (Test-Path report.html){Remove-Item report.html}
-
-
-#create HTML
-Add-Content 'report.html' '<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>File Compare</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>'
-
-
-
-
-
-Add-Content 'report.html' "<CENTER>Report of file comparison of <BR><a href='file://$dir1'>$dir1</a> <BR> and <BR><a href='file://$dir2'>$dir2</a></CENTER>"
-out-of-sync-table
-all-files-table
-
-
-
-#open the report
-Invoke-Item report.html
